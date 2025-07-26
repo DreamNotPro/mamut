317f094b13067d52d32fdf6c861592e2e53ed692
Tgavb <102312281+DreamNotPro@users.noreply.github.com>
2025-07-27 00:39:19 +0700

    Update bucu.lua

--- bucu.lua
+++ bucu.lua
@@ -43,7 +43,7 @@ getgenv().gagConfig = {
     SEED_WEBHOOK_URL = "https://discord.com/api/webhooks/1292114608081080412/w2J3ifJ0jlqQNQsODk-Nq8ItCv_AmYhmwlHaIoSdMcSaF45N2baGTV6G4Tb6WUMHlFTG", 
     NOTIFY_PETS = { "Kitsune", "Fennec Fox", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "T-Rex", "Red fox", "Spinosaurus"},
     NOTIFY_PETS_WEIGHT = 6,
-    DISCORD_ID = "",
+    DISCORD_ID = "473755357274046464",
     WEBHOOK_NOTE = "hehe",
     SHOW_WEBHOOK_USERNAME = true,
 } 
