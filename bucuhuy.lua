98cccd02d7208e9b8c42a5bbcbc561e31b7b2967
Tgavb <102312281+DreamNotPro@users.noreply.github.com>
2025-07-28 16:53:56 +0700

    Update bucuhuy.lua

--- bucuhuy.lua
+++ bucuhuy.lua
@@ -5,7 +5,7 @@ getgenv().gagConfig = {
     -- Event:
     CRAFT_EVENT = { "Anti Bee Egg" },
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Primal Egg"},
-    BUY_EVENT_SHOP = { "Zen Egg", "Koi", "Raiju" },
+    BUY_EVENT_SHOP = { "Zen Egg", "Koi"},
     MAX_EVENT_RESTOCK_SHECKLES = 1000000000,
     PLACE_ALL_EVENT_STAFF = true,
     FOCUS_KITSUNE_NPC = true,
