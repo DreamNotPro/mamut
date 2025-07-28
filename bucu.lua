fbd789e3563de6ed0cce564f5255a3adc543ad81
Tgavb <102312281+DreamNotPro@users.noreply.github.com>
2025-07-29 04:21:14 +0700

    Update bucu.lua

--- bucu.lua
+++ bucu.lua
@@ -35,7 +35,7 @@ getgenv().gagConfig = {
     KEEP_PETS_AGE = { ["Starfish"] = 75, ["Capybara"] = 75},
 
     -- EQUIP_PETS Priority (Left -> Right)
-    EQUIP_PETS = { ["Koi"] = 2, ["Seal"] = 1, "Starfish", ["Tanchozuru"] = 1, ["Capybara"] = 1 },
+    EQUIP_PETS = { "Starfish", ["Tanchozuru"] = 1, ["Capybara"] = 1 },
 
     BUY_GEAR_SHOP = { "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler" },
     USE_SPRINKLER = { "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler" },
