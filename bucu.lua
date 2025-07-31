1df30d4277a47a1900142394ed6768e58ef9b715
Tgavb <102312281+DreamNotPro@users.noreply.github.com>
2025-07-31 09:13:14 +0700

    Update bucu.lua

--- bucu.lua
+++ bucu.lua
@@ -30,7 +30,7 @@ getgenv().gagConfig = {
     FAVOURITE_FRUIT_MUTATIONS = {},  -- Stop Autosell
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
 
-    KEEP_PETS = {"Corrupted Kodama", ["Kodama"] = 5, "Corrupted Kitsune", "Raiju", ["Starfish"] = 5,["Tanuki"] = 1, ["Ostrich"] = 1, "Tanchozuru", ["Seal"] = 1, "Kitsune", "Bald Eagle", "Fennec Fox", "Blood Kiwi", ["Peacock"] = 1, "Rooster", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox", "Chicken Zombie" },
+    KEEP_PETS = {"Corrupted Kodama", ["Kodama"] = 5, "Corrupted Kitsune", "Raiju", ["Starfish"] = 5, "Tanchozuru", "Kitsune", "Bald Eagle", "Fennec Fox", "Blood Kiwi", "Rooster", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox", "Chicken Zombie" },
     KEEP_PETS_WEIGHT = 6,
     KEEP_PETS_AGE = { ["Starfish"] = 75, ["Capybara"] = 75},
 
